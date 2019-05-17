1c2d41c8e1b58a923d5f57921fdce6bf79e0a055
Viktorija Valsoe <wiktorija.valso@medieinstitutet.se>
2019-05-17 13:55:46 +0200

    olivia.hanssonlindblom@medieinstitutet.se parmis.kamali@medieinstitutet.se

---
+++ mockData.js
@@ -0,0 +1,19 @@
+const cats = [
+  { name: 'Fluffy', color: 'White', age: 3 },
+  { name: 'Aslan', color: 'Gold', age: 11 },
+  { name: 'Kitty', color: 'Grey', age: 1 },
+];
+
+const dogs = [
+  { name: 'Paddington', color: 'White', age: 1 },
+  { name: 'Winston', color: 'Black', age: 12 },
+  { name: 'Harry', color: 'Grey', age: 3 },
+];
+
+const pokemons = [
+  { name: 'Baulbazar', color: 'Green', age: 3 },
+  { name: 'Pikachu', color: 'Yellow', age: 11 },
+  { name: 'Rattata', color: 'Purple', age: 1 },
+];
+
+export default [cats, dogs, pokemons];
